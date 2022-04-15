67b87fb53b449b24666317e2c35d07f95ea4efd4
ChristineORmom <99839915+ChristineORmom@users.noreply.github.com>
2022-04-14 23:10:35 -0700

    upda

--- delete.sql
+++ delete.sql
@@ -1,10 +1,20 @@
--- INSERT INTO animals ( name, type, age ) 
--- VALUES ('Leo', 'lion', 12),
--- ('Jerry', 'mouse', 4),
--- ('Marty', 'zebra', 10),
--- ('Gloria', 'hippo', 8),
--- ('Alex', 'lion', 9),
--- ('Melman', 'giraffe', 15),
--- ('Nala', 'lion', 2),
--- ('Marie', 'cat', 1),
--- ('Flounder', 'fish', 8);
+--1 create table
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    type VARCHAR(50),
+    age INTEGER
+);
+
+
+
+INSERT INTO animals ( name, type, age ) 
+ VALUES ('Leo', 'lion', 12),
+ ('Jerry', 'mouse', 4),
+ ('Marty', 'zebra', 10),
+('Gloria', 'hippo', 8),
+('Alex', 'lion', 9),
+ ('Melman', 'giraffe', 15),
+('Nala', 'lion', 2),
+('Marie', 'cat', 1),
+'Flounder', 'fish', 8);
